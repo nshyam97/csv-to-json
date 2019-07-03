356a4c5ba6b8742f68f511398e4bb24f5cff6a27
Nish Shyam <nishanthshyam@Nishs-MacBook-Pro.local>
2019-07-03 11:05:08 +0100

    Changed the converter to create json in given format organised by station name

--- convert.py
+++ convert.py
@@ -1,10 +1,25 @@
 import pandas as pd
 import sys
+from collections import OrderedDict
+import json
 
 
 filePath = sys.argv[1]
 
 
-df = pd.read_csv(str(filePath))
+df = pd.read_csv(str(filePath), dtype={
+    "Station": str,
+    "Latitude": float,
+    "Longitude": float
+})
 
-df.to_json('data.json')
+results = []
+
+for (station), bag in df.groupby(["Station"]):
+    contents_df = bag.drop(["Station"], axis=1)
+    location = [OrderedDict(row) for i, row in contents_df.iterrows()]
+    results.append(OrderedDict([("Station", station),
+                                ("Location", location)]))
+    with open('ExpectedJsonFile.json', 'w') as outfile:
+        outfile.write(json.dumps(results, indent=4))
+
